2072962036f8660e6d3199572141815fd37bf583
gouzhun <gouzhun@nexa-corp.com>
2019-08-08 21:31:03 +0800

    first script

---
+++ Assets/scripts/SortingOrder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrder : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        int[] numbers = new[] {1, 3, 65, 2, 36, 23, 532};
+        
+        int[] targetNumbers = new int[numbers.Length];
+
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            
+            
+            Debug.Log(numbers[i]);
+        }
+
+        //----------------------
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //-----------------------
+        
+        
+
+
+        for (int i = 0; i < targetNumbers.Length; i++)
+        {
+            Debug.LogError(targetNumbers[i]);
+        }
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
